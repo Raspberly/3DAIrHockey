5e712aa61f7b8d9700bf60257c7fd52e4fe151b3
Raspberly <raspberly.unity@gmail.com>
2019-06-29 19:38:56 +0900

    反射エフェクトとヒットサウンドの追加

---
+++ Assets/Rasp/Scripts/HitSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public AudioClip clip;
+    public AudioSource source;
+
+    public void OnCollisionEnter (Collision collision)
+    {
+        //これはひどい
+        //if(collision.gameObject.name == "Ball")
+        //{
+            source.PlayOneShot(clip);
+        //}
+    }
+}
