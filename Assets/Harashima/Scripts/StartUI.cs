5457803d87456e079c3a8cb09445105e05c0ae0e
hika04157726@gmail.com <hika.04157726@gmail.com>
2019-06-29 18:33:13 +0900

    StartUIの実装

---
+++ Assets/Harashima/Scripts/StartUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartUI : MonoBehaviour
+{
+    public GameObject startUI;
+    private bool isStart = true;
+
+    [SerializeField]
+    private float speed = 10f;
+
+    void Update() {
+        startUI.transform.Translate(Vector3.right * (-1) * Time.deltaTime * speed);
+    }
+
+    /*
+    public void DisplayStartUI() {
+        Color color = startText.color;
+        color.a = color.a <= 0 ? 1 : color.a - 0.01f;
+        startText.color = color;
+        if (color.a == 0) isStart = false;
+    }*/
+}
