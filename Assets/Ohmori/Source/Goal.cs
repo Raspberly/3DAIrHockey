692b63111819c5cb914301b96fb8e0d2eb50ffac
ohmori <houki1221@yahoo.co.jp>
2019-06-29 17:38:06 +0900

    繁栄

--- Assets/Ohmori/Source/Goal.cs
+++ Assets/Ohmori/Source/Goal.cs
@@ -15,18 +15,35 @@ public class Goal : MonoBehaviour
     //玉がゴールに触れた際のパーティクル
     [SerializeField]
     private GameObject outParticle;
+    //ボール
+    [SerializeField]
+    private GameObject ball;
+
+    void Update()
+    {
+
+    }
 
     /// <summary>
     /// 当たり判定
     /// </summary>
     void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Hit:"+ other.transform.name);
         if (other.tag =="ball")
         {
             Instantiate(outParticle, other.gameObject.transform);
             Destroy(other.gameObject);
             //ポイントを加算
             systemController.SetPoint(pointGetTarget);
+            ReStart();
         }
     }
+
+    //再復帰
+    void ReStart()
+    {
+        Instantiate(ball);
+    }
+    
 }
