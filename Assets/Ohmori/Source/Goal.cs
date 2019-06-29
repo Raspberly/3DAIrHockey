953683bc7890cb80406e0ca43a7e53493b611525
ohmori <houki1221@yahoo.co.jp>
2019-06-29 16:37:25 +0900

    ゴールという概念

---
+++ Assets/Ohmori/Source/Goal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 後ろの壁にあたった際の当たり判定
+/// </summary>
+public class Goal : MonoBehaviour
+{
+    //誰がポイントを獲得するか
+   [SerializeField]
+    private GAME.TARGET pointGetTarget;
+    [SerializeField]
+    private SystemController systemController;
+    //玉がゴールに触れた際のパーティクル
+    [SerializeField]
+    private GameObject outParticle;
+
+    /// <summary>
+    /// 当たり判定
+    /// </summary>
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag =="ball")
+        {
+            Instantiate(outParticle, other.gameObject.transform);
+            Destroy(other.gameObject);
+            //ポイントを加算
+            systemController.SetPoint(pointGetTarget);
+        }
+    }
+}
